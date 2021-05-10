c94b8990a2429975acd50b70ac0e66ef0ed24e7d
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-05-10 16:26:36 -0700

    add test for Pastry constructor

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -14,6 +14,13 @@ namespace Bakery.Tests
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
 
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(1);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+
   }
 }
 
