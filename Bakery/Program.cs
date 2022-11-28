2a146e3d7f2c34065a807fda47b956bfd1ef3d9a
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2022-11-27 18:25:36 -0800

    update bread branch/tweak some ui msgs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -21,7 +21,6 @@ public class Program
       int userQuantityInt = Convert.ToInt32(userQuantityString);
       Bread breadOrder = new Bread(userQuantityInt);
       breadOrder.GetPrice();
-      breadOrder.BreadDeal();
       Console.WriteLine(breadOrder.Quantity + " loaves of bread will cost you $" + breadOrder.TotalPrice + ".");
       Console.WriteLine("------------------");
       Main();
@@ -46,7 +45,7 @@ public class Program
     else
     {
       Console.WriteLine("------------------------");
-      Console.WriteLine("Oops! That is an invalid choice. Please exit the store and Return when you want some tasty baked goods.");
+      Console.WriteLine("Oops! That is an invalid choice. Please grab another ticket and head to the back of the line.");
       Console.WriteLine("------------------------");
       Main();
     }
@@ -54,10 +53,10 @@ public class Program
   private static void WelcomeGreeting()
   {
     Console.WriteLine("------------------");
-    Console.WriteLine("Hello, and welcome back to Giancarlo's Bakery!");
+    Console.WriteLine("NEXT!!!");
+    Console.WriteLine("Oh hello, and welcome back to Giancarlo's Bakery!");
     Console.WriteLine("We offer a fine selection of Bread and Pastries.");
     Console.WriteLine("Bread is $5 per loaf but if you LOVE bread we have a deal for you: Buy 2, and get 1 free.");
-    Console.WriteLine("(Please note: You must accept the extra loaves of bread offered in our 2 for 1 bread deal.)");
     Console.WriteLine("Pastries are $2 each or 3 for $5.");
     Console.WriteLine("Please enter; 'Bread' or 'Pastry' if you would like to purchase either bread or pastry respectively. Or if you are done enter the word 'Exit'.");
   }
